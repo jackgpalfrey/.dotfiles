a00ac92895dc9e5c7b8e14734218335286a755cb
Jack Palfrey <jackgpalfrey@outlook.com>
2024-02-28 13:56:04 +0000

    feat: update harpoon

--- home/.config/nvim/lua/jackgpalfrey/plugins/harpoon.lua
+++ home/.config/nvim/lua/jackgpalfrey/plugins/harpoon.lua
@@ -2,18 +2,20 @@ local harpoon = function()
     return require("harpoon")
 end
 
-
 return {
     "ThePrimeagen/harpoon",
     branch = "harpoon2",
     dependencies = { "nvim-lua/plenary.nvim" },
-    opts = { },
-    config = config,
+    opts = {
+        settings = {
+            save_on_toggle = false
+        }
+    },
     keys = {
-        { "<leader>a", function() harpoon():list():append() end, desc = "[A]dd current buffer to harpoon" },
-        { "<C-e>", function() harpoon().ui:toggle_quick_menu(harpoon():list()) end, desc = "Show harpoon" },
+        { "<leader>a", function() harpoon():list():append() end,  desc = "[A]dd current buffer to harpoon" },
+        { "<C-e>",     function() harpoon().ui:toggle_quick_menu(harpoon():list()) end, desc = "Show harpoon" },
 
-        { "<leader>1", function() harpoon():list():select(1) end, desc = "which_key_ignore"},
+        { "<leader>1", function() harpoon():list():select(1) end, desc = "which_key_ignore" },
         { "<leader>2", function() harpoon():list():select(2) end, desc = "which_key_ignore" },
         { "<leader>3", function() harpoon():list():select(3) end, desc = "which_key_ignore" },
         { "<leader>4", function() harpoon():list():select(4) end, desc = "which_key_ignore" },
